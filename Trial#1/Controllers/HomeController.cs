c939f74a02a223480cb9aace6eafca3414852f61
konst <konst@DESKTOP-2ATH6DA>
2025-04-27 20:07:42 +0300

    Add project files.

---
+++ Trial#1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Trial_1.Models;
+
+namespace Trial_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+            ViewData["Message"] = "Information about MultiSoftware Enterprise.";
+            return View();
+        }
+
+    }
+}
